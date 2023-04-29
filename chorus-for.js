1fbc7cf397f8286bbae3f98a6bd23b538aa3e0f5
Gordenguan <gordenguan122@gmail.com>
2023-04-28 22:24:55 -0600

    for

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,18 @@
-const chorus = "Let's dance!";
-for (let repeat = 0; repeat < 10; repeat++) {
-  console.log(chorus);
+// const chorus = "Let's dance!";
+// for (let repeat = 0; repeat < 10; repeat++) {
+//   console.log(chorus);
+// }
+// console.log("Until the sun comes up!");
+
+
+for (let i = 100; i < 201; i++){
+  if (i % 3 === 0 && i % 4 === 0) {
+    console.log('LoopyLighthouse')
+  } else if (i % 4 === 0) {
+    console.log('Lighthouse')
+  } else if (i % 3 === 0 ) {
+    console.log('Loopy')
+  } else {
+    console.log(i)
+  }
 }
-console.log("Until the sun comes up!");
